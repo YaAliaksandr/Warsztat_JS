460c85fc7ac85cede27ddfab87cfffffc10ec0f0
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-21 09:21:35 +0200

    co

--- js/myMain.js
+++ js/myMain.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const inputsRef = document.querySelectorAll('.calc__input');
+const divForDrop = document.querySelector('.select__input');
+
+const liResult = document.querySelectorAll('.list__item ');
+
+
+
+console.log(liResult)
+
+// liResult.forEach((li) => this.style.display = "block");
+console.log(inputsRef);
+
+console.log(divForDrop);
+
+
+inputsRef.forEach((input) => input.addEventListener('click',))
